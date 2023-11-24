803e754b764718ee894027655cb3cb961d7dfa0f
Jayant <23m2112@iitb.ac.in>
2023-11-24 17:40:41 +0530

    data fetch

---
+++ website/xampp/htdocs/proj/pbag.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "users";
+
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$item_name = 'bag'; 
+$sql = "SELECT price FROM items WHERE name = '$item_name'";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+   
+    $price = $row['price'];
+    
+    echo "The price of $item_name is: $" . $price;
+} else {
+    echo "Item not found!";
+}
+
+mysqli_close($conn);
+?>
